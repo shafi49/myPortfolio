659847a68638096f1c476a68b4b0bd0410f69935
SHAFI <shafiul.shafi@gmail.com>
2015-11-23 16:25:59 +0600

    custom post type and fields deleted
    Something was wrong... Planning to start over.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
       <!--we could also use WP_Query() for more precise loop-->
        <?php if (have_posts()) : while (have_posts()) : the_post ();?>
         <div class="leader">
+          
           <a href="<?php the_permalink();?>"><?php the_title('<h1>', '</h1>');?></a>
           <?php the_content();?>    <!-- default <p> tags around the_content()-->  
         </div>
